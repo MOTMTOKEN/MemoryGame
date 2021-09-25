13b985fda41c26e8b0ce78dcc41bbcb886b0567e
amirnilsson <amirnilsson@gmail.com>
2021-09-25 15:55:15 +0200

    Added background animation with gradient colors.

--- app/src/main/java/com/example/gamecardexam/MainActivity.kt
+++ app/src/main/java/com/example/gamecardexam/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.gamecardexam
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var myLayout = findViewById<ConstraintLayout>(R.id.Background_id)
+        val animDrawable = myLayout.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(2500)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
 
         val buttonOne = findViewById<Button>(R.id.instBtn)
         val buttonPlay = findViewById<Button>(R.id.playBtn)
